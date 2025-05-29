50af0135dfa940788094cffbec903be718cd68c3
Takhir Galimzianov <tariel@post.com>
2025-05-29 23:57:48 +0400

    Добавил новый проект для клиента gRPC, и сопутстввующие фалы, настроил все.

---
+++ Translit.ConsoleClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using System.Text;
+using Translit.Service.Grpc;
+using static Translit.Service.Grpc.Translit;
+
+namespace Translit.ConsoleClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:7041");
+            var client = new TranslitClient(channel);
+
+            Console.WriteLine("Введите фразу для транслитерации:");
+            while (true)
+            {
+                Console.Write("> ");
+                var phrase = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(phrase)) break;
+
+                Console.WriteLine($"Введено: {phrase}");
+                var response = await client.TransliterateAsync(new TranslitRequest { Phrase = phrase });
+                Console.WriteLine($"Транслитерация: {response.Result}");
+
+            }
+        }
+    }
+}
--- Translit.Service/Services/TranslitService.cs
+++ Translit.Service/Services/TranslitService.cs
@@ -1,4 +1,7 @@
-﻿using Translit.Shared.Interfaces;
+﻿using Grpc.Core;
+using System.Text;
+using Translit.Service.Grpc;
+using Translit.Shared.Interfaces;
 
 namespace Translit.Service.Services
 {
@@ -6,28 +9,31 @@ namespace Translit.Service.Services
     {
         private static readonly Dictionary<char, string> _map = new()
         {
-            ['а'] = "a", ['б'] = "b", ['в'] = "v",['г'] = "g", ['д'] = "d",
-            ['е'] = "e", ['ё'] = "yo", ['ж'] = "zh", ['з'] = "z", ['и'] = "i",
-            ['й'] = "y", ['к'] = "k", ['л'] = "l", ['м'] = "m", ['н'] = "n",
-            ['о'] = "o", ['п'] = "p", ['р'] = "r",['с'] = "s", ['т'] = "t",
-            ['у'] = "u", ['ф'] = "f", ['х'] = "kh",['ц'] = "ts", ['ч'] = "ch",
-            ['ш'] = "sh",['щ'] = "shch",['ъ'] = "", ['ы'] = "y", ['ь'] = "",
-            ['э'] = "e", ['ю'] = "yu", ['я'] = "ya"
+            ['А'] = "A",['Б'] = "B",['В'] = "V",['Г'] = "G",['Д'] = "D",['Е'] = "E",['Ё'] = "E",['Ж'] = "Zh",
+            ['З'] = "Z",['И'] = "I",['Й'] = "Y",['К'] = "K",['Л'] = "L",['М'] = "M",['Н'] = "N",['О'] = "O",
+            ['П'] = "P",['Р'] = "R",['С'] = "S",['Т'] = "T",['У'] = "U",['Ф'] = "F",['Х'] = "Kh",['Ц'] = "Ts",
+            ['Ч'] = "Ch",['Ш'] = "Sh",['Щ'] = "Shch",['Ъ'] = "",['Ы'] = "Y",['Ь'] = "",['Э'] = "E",['Ю'] = "Yu",
+            ['Я'] = "Ya",['а'] = "a",['б'] = "b",['в'] = "v",['г'] = "g",['д'] = "d",['е'] = "e",['ё'] = "e",
+            ['ж'] = "zh",['з'] = "z",['и'] = "i",['й'] = "y",['к'] = "k",['л'] = "l",['м'] = "m",['н'] = "n",
+            ['о'] = "o",['п'] = "p",['р'] = "r",['с'] = "s",['т'] = "t",['у'] = "u",['ф'] = "f",['х'] = "kh",
+            ['ц'] = "ts",['ч'] = "ch",['ш'] = "sh",['щ'] = "shch",['ъ'] = "",['ы'] = "y",['ь'] = "",['э'] = "e",['ю'] = "yu",['я'] = "ya"
         };
 
         public string Transliterate(string phrase)
         {
-            if (string.IsNullOrWhiteSpace(phrase))
-                return string.Empty;
+            Console.WriteLine($"[Service] Входная фраза: {phrase}");
 
-            var result = new List<string>();
-
-            foreach (var ch in phrase.ToLowerInvariant())
+            var result = new StringBuilder();
+            foreach (var c in phrase)
             {
-                result.Add(_map.TryGetValue(ch, out var latin) ? latin : ch.ToString());
+                if (_map.TryGetValue(c, out var latin))
+                    result.Append(latin);
+                else
+                    result.Append(c);
             }
-
-            return string.Join("", result);
+            var output = result.ToString();
+            Console.WriteLine($"[Service] Результат: {output}");
+            return output;
         }
     }
 }
