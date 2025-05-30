9cf538ccb7be76ad446b38ad63a71e1056c685c9
Takhir Galimzianov <tariel@post.com>
2025-05-30 13:25:21 +0400

    Добавил новый проект REST API чтобы можно было напрямую в строке браузера можно получить транслитирование

---
+++ Translit.RestApi/Controllers/TranslitController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Translit.Shared.Interfaces;
+
+namespace Translit.RestApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TranslitController : ControllerBase
+    {
+        private readonly ITranslitService _service;
+
+        public TranslitController(ITranslitService service)
+        {
+            _service = service;
+        }
+
+
+        // Этот метод можно вызывать из браузера
+        [HttpGet("transliterate")]
+        public ActionResult<string> Transliterate([FromQuery] string phrase)
+        {
+            if (string.IsNullOrWhiteSpace(phrase))
+                return BadRequest("Пустая фраза");
+
+            var result = _service.Transliterate(phrase);
+            return Ok(result);
+        }
+
+
+    }
+}
---
+++ Translit.RestApi/Program.cs
@@ -0,0 +1,31 @@
+using Translit.Service.Services;
+using Translit.Shared.Interfaces;
+
+namespace Translit.RestApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddScoped<ITranslitService, TranslitService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
